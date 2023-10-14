c2ef8dd3dfa17155c87226b461302b26cb000cec
Luizth25 <luizftp25@gmail.com>
2023-10-14 15:32:44 -0300

    new questions

--- src/questions/index.ts
+++ src/questions/index.ts
@@ -17,137 +17,137 @@ const questions = [
     answer: "Compara dois valores para igualdade estrita",
   },
 
-  // {
-  //   question: "Como você declara uma variável em JavaScript?",
-  //   options: [
-  //     "var myVar;",
-  //     "variable = myVar;",
-  //     "myVar = new Variable();",
-  //     "variável = 42;",
-  //   ],
-  //   answer: "var myVar;",
-  // },
+  {
+    question: "Como você declara uma variável em JavaScript?",
+    options: [
+      "var myVar;",
+      "variable = myVar;",
+      "myVar = new Variable();",
+      "variável = 42;",
+    ],
+    answer: "var myVar;",
+  },
 
-  // {
-  //   question: "O que é um array em JavaScript?",
-  //   options: [
-  //     "Uma estrutura de dados para armazenar múltiplos valores",
-  //     "Um tipo de loop",
-  //     "Um método para escrever comentários",
-  //     "Uma classe para criar objetos",
-  //   ],
-  //   answer: "Uma estrutura de dados para armazenar múltiplos valores",
-  // },
+  {
+    question: "O que é um array em JavaScript?",
+    options: [
+      "Uma estrutura de dados para armazenar múltiplos valores",
+      "Um tipo de loop",
+      "Um método para escrever comentários",
+      "Uma classe para criar objetos",
+    ],
+    answer: "Uma estrutura de dados para armazenar múltiplos valores",
+  },
 
-  // {
-  //   question: "Qual é a função de 'console.log()' em JavaScript?",
-  //   options: [
-  //     "Registrar um usuário no console",
-  //     "Imprimir algo na tela",
-  //     "Realizar cálculos matemáticos",
-  //     "Criar um novo arquivo",
-  //   ],
-  //   answer: "Imprimir algo na tela",
-  // },
+  {
+    question: "Qual é a função de 'console.log()' em JavaScript?",
+    options: [
+      "Registrar um usuário no console",
+      "Imprimir algo na tela",
+      "Realizar cálculos matemáticos",
+      "Criar um novo arquivo",
+    ],
+    answer: "Imprimir algo na tela",
+  },
 
-  // {
-  //   question: "O que é uma função em JavaScript?",
-  //   options: [
-  //     "Um tipo de loop",
-  //     "Um objeto de data",
-  //     "Um bloco de código reutilizável",
-  //     "Uma variável global",
-  //   ],
-  //   answer: "Um bloco de código reutilizável",
-  // },
+  {
+    question: "O que é uma função em JavaScript?",
+    options: [
+      "Um tipo de loop",
+      "Um objeto de data",
+      "Um bloco de código reutilizável",
+      "Uma variável global",
+    ],
+    answer: "Um bloco de código reutilizável",
+  },
 
-  // {
-  //   question: "O que é o DOM em JavaScript?",
-  //   options: [
-  //     "Um tipo de dado",
-  //     "Uma estrutura de controle de fluxo",
-  //     "A representação da estrutura HTML da página",
-  //     "Um método matemático",
-  //   ],
-  //   answer: "A representação da estrutura HTML da página",
-  // },
+  {
+    question: "O que é o DOM em JavaScript?",
+    options: [
+      "Um tipo de dado",
+      "Uma estrutura de controle de fluxo",
+      "A representação da estrutura HTML da página",
+      "Um método matemático",
+    ],
+    answer: "A representação da estrutura HTML da página",
+  },
 
-  // {
-  //   question: "Qual é o operador de adição em JavaScript?",
-  //   options: ["+", "-", "*", "/"],
-  //   answer: "+",
-  // },
+  {
+    question: "Qual é o operador de adição em JavaScript?",
+    options: ["+", "-", "*", "/"],
+    answer: "+",
+  },
 
-  // {
-  //   question: "O que é um evento em JavaScript?",
-  //   options: [
-  //     "Um erro no código",
-  //     "Uma função interna do navegador",
-  //     "Uma ação que ocorre na página, como um clique do mouse",
-  //     "Um comentário no código",
-  //   ],
-  //   answer: "Uma ação que ocorre na página, como um clique do mouse",
-  // },
+  {
+    question: "O que é um evento em JavaScript?",
+    options: [
+      "Um erro no código",
+      "Uma função interna do navegador",
+      "Uma ação que ocorre na página, como um clique do mouse",
+      "Um comentário no código",
+    ],
+    answer: "Uma ação que ocorre na página, como um clique do mouse",
+  },
 
-  // {
-  //   question: "O que é um loop 'for' em JavaScript usado para?",
-  //   options: [
-  //     "Declarar variáveis",
-  //     "Definir funções",
-  //     "Iterar sobre uma coleção de elementos",
-  //     "Criar condicionais",
-  //   ],
-  //   answer: "Iterar sobre uma coleção de elementos",
-  // },
-  // {
-  //   question: "Qual palavra-chave é usada para declarar uma variável em C#?",
-  //   options: ["var", "int", "string", "float"],
-  //   answer: "var",
-  // },
+  {
+    question: "O que é um loop 'for' em JavaScript usado para?",
+    options: [
+      "Declarar variáveis",
+      "Definir funções",
+      "Iterar sobre uma coleção de elementos",
+      "Criar condicionais",
+    ],
+    answer: "Iterar sobre uma coleção de elementos",
+  },
+  {
+    question: "Qual palavra-chave é usada para declarar uma variável em C#?",
+    options: ["var", "int", "string", "float"],
+    answer: "var",
+  },
 
-  // {
-  //   question: "O que é um método em C#?",
-  //   options: [
-  //     "Um objeto de data",
-  //     "Um bloco de código reutilizável",
-  //     "Uma variável global",
-  //     "Uma estrutura de controle de fluxo",
-  //   ],
-  //   answer: "Um bloco de código reutilizável",
-  // },
+  {
+    question: "O que é um método em C#?",
+    options: [
+      "Um objeto de data",
+      "Um bloco de código reutilizável",
+      "Uma variável global",
+      "Uma estrutura de controle de fluxo",
+    ],
+    answer: "Um bloco de código reutilizável",
+  },
 
-  // {
-  //   question: "Qual é a finalidade da palavra-chave 'using' em C#?",
-  //   options: [
-  //     "Declarar uma variável",
-  //     "Importar namespaces",
-  //     "Definir funções",
-  //     "Criar condicionais",
-  //   ],
-  //   answer: "Importar namespaces",
-  // },
+  {
+    question: "Qual é a finalidade da palavra-chave 'using' em C#?",
+    options: [
+      "Declarar uma variável",
+      "Importar namespaces",
+      "Definir funções",
+      "Criar condicionais",
+    ],
+    answer: "Importar namespaces",
+  },
 
-  // {
-  //   question: "O que é a compilação em C#?",
-  //   options: [
-  //     "Um tipo de loop",
-  //     "Um método matemático",
-  //     "O processo de transformar código-fonte em código executável",
-  //     "Uma estrutura de dados para armazenar múltiplos valores",
-  //   ],
-  //   answer: "O processo de transformar código-fonte em código executável",
-  // },
+  {
+    question: "O que é a compilação em C#?",
+    options: [
+      "Um tipo de loop",
+      "Um método matemático",
+      "O processo de transformar código-fonte em código executável",
+      "Uma estrutura de dados para armazenar múltiplos valores",
+    ],
+    answer: "O processo de transformar código-fonte em código executável",
+  },
 
-  // {
-  //   question: "O que é um construtor em C#?",
-  //   options: [
-  //     "Um comentário no código",
-  //     "Uma função interna do navegador",
-  //     "Um método especial para inicializar objetos",
-  //     "Um erro no código",
-  //   ],
-  //   answer: "Um método especial para inicializar objetos",
-  // },
+  {
+    question: "O que é um construtor em C#?",
+    options: [
+      "Um comentário no código",
+      "Uma função interna do navegador",
+      "Um método especial para inicializar objetos",
+      "Um erro no código",
+    ],
+    answer: "Um método especial para inicializar objetos",
+  },
 ];
 
 export default questions;
